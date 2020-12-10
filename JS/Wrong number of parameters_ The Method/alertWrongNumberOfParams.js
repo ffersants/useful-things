ad25bdaf0db1abb7a7d3bec9ef9dd1d9ff9afa79
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2020-12-09 23:54:13 -0300

    Criado método para verificar se os parâmetros esperados eram passados e para apresentar um erro caso a condição não tenha sido atingida.

---
+++ JS/Wrong number of parameters_ The Method/alertWrongNumberOfParams.js
@@ -0,0 +1,18 @@
+
+function showName(nome, sobrenome){
+    checkArgs(2, 'nome, sobrenome')
+    console.log(nome, sobrenome);
+}
+
+function checkArgs(numOfParamsExpected, ...paramsExpected){
+    if(checkArgs.caller.arguments.length !== numOfParamsExpected){
+        alertWrongNumberOfParams(numOfParamsExpected, paramsExpected)
+    }
+}
+
+function alertWrongNumberOfParams(numOfParams, paramNames){
+    throw new Error('Error: ' + numOfParams + ' arguments are expected: ' + paramNames + '.')
+}
+//showName('Fernando');
+//showName('Fernando', 'Santos', 'Ferreira');
+showName('Fernando', 'Santos Ferreira',);
